fb0596ec6c105b4afbeb9aacdf0b12ffa800eeff
EduardoEinsfeldt <eduardoa.einsfeldt@gmail.com>
2025-03-18 11:27:56 -0300

    Aula 18/03

---
+++ api-rest/src/main/java/br/com/fiap/api_rest/model/Endereco.java
@@ -0,0 +1,29 @@
+package br.com.fiap.api_rest.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String localizacao;
+    @OneToOne(mappedBy = "endereco")
+    private Filial filial;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+}
